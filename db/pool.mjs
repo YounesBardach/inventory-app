3b7596521fd69615ce942725ef3c39f263611318
younesbardach <85373688+YounesBardach@users.noreply.github.com>
2025-02-23 03:34:12 -0500

    Changed pool Railway URL

--- db/pool.mjs
+++ db/pool.mjs
@@ -2,9 +2,17 @@ import "../config.mjs";
 import pkg from "pg";
 const { Pool } = pkg;
 
+// export const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: parseInt(process.env.DB_PORT) || 5432,
+// });
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, // Necessary for Railway's managed PostgreSQL
+    rejectUnauthorized: false, // Necessary for Railway's PostgreSQL SSL connection
   },
 });
